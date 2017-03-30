71270c6a15307701bd3b44fe8b9b69daa8a451fe
Yuri Vasconcelos Asserth <yuri.vasconcelos@grupoasserth.com.br>
2017-03-30 11:04:27 -0300

    Ajustes 30/03

---
+++ src/VDT2/SQL 0.0.4/Tabela CustoReparo.sql
@@ -0,0 +1,25 @@
+use VDT2
+GO
+
+if Exists(select 1 
+            from sys.objects 
+            where name like '%CustoReparo%')
+
+BEGIN
+  Truncate Table CustoReparo
+  Drop Table CustoReparo
+END
+
+--------------------------------------------------------------------------------
+-- Custos Reparo
+-- Criação: 30/03
+--------------------------------------------------------------------------------
+Create Table CustoReparo (
+  CustoReparo_ID    Int Not Null Identity Primary Key,
+  AvArea_ID         Int Foreign Key References AvArea(AvArea_ID),
+  AvGravidade_ID    Int Foreign Key References AvGravidade(AvGravidade_ID),
+  Custo             Decimal (8,2)
+)
+
+
+-- Select * from CustoReparo
