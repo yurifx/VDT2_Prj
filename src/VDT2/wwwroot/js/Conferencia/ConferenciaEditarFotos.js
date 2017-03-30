5f28b05b2064e25a9b1aa6738d9aa3eb12729a13
Yuri Vasconcelos Asserth <yuri.vasconcelos@grupoasserth.com.br>
2017-03-30 09:24:54 -0300

    30/03 - Ajustes nas fotos e permissões

--- src/VDT2/wwwroot/js/Conferencia/ConferenciaEditarFotos.js
+++ src/VDT2/wwwroot/js/Conferencia/ConferenciaEditarFotos.js
@@ -167,24 +167,6 @@
         RemoverFoto(10);
     })
 
-
-    var alteraInspecao = $("#hiddenAlteraInspecao").val();
-    if (alteraInspecao == 'False ') {
-        $("#spanFoto1").hide();
-        $("#btnSalvar").hide();
-        $('#tituloPaginaVisualizarFotos').hide();
-
-        $('.btn-danger').each(function (i, e) {
-            $(e).hide();
-        });
-
-    } else {
-        $("#spanFoto1").show();
-        $("#btnSalvar").show();
-
-    }
-
-
 });
 
 
