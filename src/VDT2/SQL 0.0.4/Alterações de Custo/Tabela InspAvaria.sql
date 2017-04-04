e2dca1810f5bae5d7f344bd52100e7f54e43404c
Yuri Vasconcelos Asserth <yuri.vasconcelos@grupoasserth.com.br>
2017-04-04 17:03:00 -0300

    Scripts 0.0.4 devem ficar apenas na versao 0.0.4

--- src/VDT2/SQL 0.0.4/Alterações de Custo/Tabela InspAvaria.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------
--- Script de Alteração da tabela InspAvaria
--- Data criação: 30/03/2017
---------------------------------------------------
-
-Alter Table InspAvaria
-ADD Custo decimal (7,2)
