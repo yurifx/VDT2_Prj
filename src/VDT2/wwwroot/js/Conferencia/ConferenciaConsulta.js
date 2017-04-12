92baa6b0b313c7519e619414b50e583fa2a3eed2
Yuri Vasconcelos Asserth <yuri.vasconcelos@grupoasserth.com.br>
2017-04-12 13:26:28 -0300

    Ajustes realizados

---
+++ src/VDT2/wwwroot/js/Conferencia/ConferenciaConsulta.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    var checkboxes = $('input[type=checkbox]');
+    checkboxes.each(function (i, e) {
+
+        //$(e).attr('checked', 'checked');
+        $(e).prop('checked', true);
+    })
+
+
+})
+
