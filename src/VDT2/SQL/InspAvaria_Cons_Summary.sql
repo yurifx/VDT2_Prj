d6121a93e3d01fefb3c97264ae6b5e88b9f7d870
Yuri Vasconcelos Asserth <yuri.vasconcelos@grupoasserth.com.br>
2017-04-20 16:47:06 -0300

    Nova Procedure de summary

---
+++ src/VDT2/SQL/InspAvaria_Cons_Summary.sql
@@ -0,0 +1,219 @@
+USE [VDT2]
+GO
+
+If  Exists (Select Name
+            From   sysobjects
+            Where  Name = 'InspAvaria_Cons_Summary' and type = 'P')
+    Drop Procedure dbo.InspAvaria_Cons_Summary
+GO
+
+Create Procedure InspAvaria_Cons_Summary 
+(  
+    @p_Cliente_ID             Int,
+    @p_Chassi                 Varchar(100),
+    @p_LocalInspecao          Varchar(100),
+    @p_LocalCheckPoint        Varchar(100),
+    @p_Transportador          Varchar(100),
+	@p_Lote                   Varchar(50),
+    @p_Marca                  Varchar(100),
+    @p_Modelo                 Varchar(100),
+    @p_Area                   Varchar(100),
+    @p_Condicao               Varchar(100),
+    @p_Dano                   Varchar(100),
+    @p_Quadrante              Varchar(100),
+    @p_Gravidade              Varchar(100),
+    @p_Severidade             Varchar(100),
+    @p_TipoDefeito            Varchar(100),  --Transporte/Fábrica/Todos
+    @p_DanoOrigem             Varchar(100),  -- Sim/Não/Todos
+    @p_TransportadorTipo      Varchar(100),  -- Marítimo/Terrestre/Todos
+    @p_FrotaViagem            Varchar(100),  -- Nome da frotaviagem
+    @p_Navio                  Varchar(100),
+    @p_DataInicio             Date,
+    @p_DataFinal              Date
+)
+
+AS
+
+DECLARE @IDs table (InspVeiculo_ID int, VIN varchar(17))
+
+Insert into @IDs (InspVeiculo_ID, VIN) 
+select iv.InspVeiculo_ID, IsNull(VIN, VIN_6)From InspVeiculo iv
+Inner Join  Inspecao           i      on   iv.Inspecao_ID          =      i.Inspecao_ID
+Inner Join  Transportador      t      on    t.Transportador_ID     =      i.Transportador_ID
+Inner Join  FrotaViagem        fv     on   fv.FrotaViagem_ID       =      i.FrotaViagem_ID
+Left Join   Lote                l     on   iv.Lote_ID              =      l.Lote_ID
+Left Join   Navio               n     on    n.Navio_ID             =      i.Navio_ID 
+Left Join   InspAvaria         ia     on   iv.InspVeiculo_ID       =     ia.InspVeiculo_ID
+
+
+Where 
+
+i.Publicado = 1
+
+and  @p_Cliente_ID = i.Cliente_ID
+
+and (@p_Chassi is null
+        or iv.VIN_6  like '%' + @p_Chassi + '%')
+        
+and (@p_LocalInspecao = '*'
+        or CharIndex( '|'+ Cast(i.LocalInspecao_ID as Varchar) +'|', @p_LocalInspecao) > 0 )
+
+and (@p_LocalCheckPoint = '*'
+        or CharIndex( '|'+ Cast(i.LocalCheckPoint_ID as Varchar) +'|', @p_LocalCheckPoint) > 0 )
+
+and (@p_Transportador = '*'
+        or CharIndex( '|'+ Cast(i.Transportador_ID as Varchar) +'|', @p_Transportador) > 0 )
+
+and (@p_Marca = '*'
+        or CharIndex( '|'+ Cast(iv.Marca_ID as Varchar) +'|', @p_Marca) > 0 )
+
+and (@p_Modelo = '*'
+        or CharIndex( '|'+ Cast(iv.Modelo_ID as Varchar) +'|', @p_Modelo) > 0 )
+
+and (@p_Area = '*'
+        or CharIndex( '|'+ Cast(ia.AvArea_ID as Varchar) +'|', @p_Area) > 0 )
+
+and (@p_Condicao = '*'
+        or CharIndex( '|'+ Cast(ia.AvCondicao_ID as Varchar) +'|', @p_Condicao) > 0 )
+
+and (@p_Dano = '*'
+        or CharIndex( '|'+ Cast(ia.AvDano_ID as Varchar) +'|', @p_Dano) > 0 )
+
+and (@p_Gravidade = '*'
+        or CharIndex( '|'+ Cast(ia.AvGravidade_ID as Varchar) +'|', @p_Gravidade) > 0 )
+
+and (@p_Quadrante = '*'
+        or CharIndex( '|'+ Cast(ia.AvQuadrante_ID as Varchar) +'|', @p_Quadrante) > 0 )
+
+and (@p_Severidade = '*'
+        or CharIndex( '|'+ Cast(ia.AvSeveridade_ID as Varchar) +'|', @p_Severidade) > 0 )
+        
+and (@p_TipoDefeito = '*'
+        or CharIndex( '|'+ Cast(ia.FabricaTransporte as Varchar) +'|', @p_TipoDefeito) > 0 )
+        
+and (@p_DanoOrigem = '*'
+        or CharIndex( '|'+ Cast(ia.DanoOrigem as Varchar) +'|', @p_DanoOrigem) > 0 )
+
+and (@p_TransportadorTipo = '*'
+        or CharIndex( '|'+ Cast(t.Tipo as Varchar) +'|', @p_TransportadorTipo) > 0 )
+
+and (@p_FrotaViagem is null
+        or fv.Nome like '%' + @p_FrotaViagem + '%')
+
+and (@p_Navio is null
+        or n.Nome like '%' + @p_Navio + '%')
+
+and (@p_Lote is null
+        or l.Lote  like '%' + @p_Lote + '%')
+
+and i.Data between @p_DataInicio and @p_DataFinal
+
+
+
+select 'TodosVeiculos' as Tipo, count(distinct VIN) Total 
+from @IDs
+
+union
+
+select 'VeiculosComAvarias' as Tipo, count(distinct iv.InspVeiculo_ID) Total from inspVeiculo iv
+inner join @IDs tmp on iv.InspVeiculo_ID = tmp.InspVeiculo_ID
+inner join InspAvaria ia on iv.InspVeiculo_ID = ia.InspVeiculo_ID
+
+union
+
+select 'VeiculosSemAvaria' as Tipo, count(distinct iv.InspVeiculo_ID) Total from inspVeiculo iv
+inner join @IDs tmp on iv.InspVeiculo_ID = tmp.InspVeiculo_ID
+where not exists (select 1 from inspAvaria ia where ia.InspVeiculo_ID = iv.InspVeiculo_ID)
+
+
+union 
+
+select 'QuantidadeAvarias' as Tipo, count(inspAvaria_id) Total from InspAvaria ia
+inner join @IDs tmp on ia.InspVeiculo_ID = tmp.InspVeiculo_ID
+
+
+union 
+
+select 'QuantidadeAvariasTransporte' as Tipo, count(inspAvaria_id) Total from InspAvaria ia
+inner join @IDs tmp on ia.InspVeiculo_ID = tmp.InspVeiculo_ID
+where ia.FabricaTransporte = 'T'
+
+
+union
+
+select 'QuantidadeAvariasFabrica' as Tipo, count(inspAvaria_id) Total from InspAvaria ia
+inner join @IDs tmp on ia.InspVeiculo_ID = tmp.InspVeiculo_ID
+where ia.FabricaTransporte = 'F'
+
+
+/*
+Declare @p_Cliente_ID             Int,
+        @p_Chassi                 varchar(100),                                             
+        @p_LocalInspecao          varchar(100),                                             
+        @p_LocalCheckPoint        varchar(100),
+        @p_Transportador          varchar(100),
+        @p_Lote                   Varchar(50),                                             
+        @p_Marca                  varchar(100),                                             
+        @p_Modelo                 varchar(100),                                             
+        @p_Area                   varchar(100),                                             
+        @p_Condicao               varchar(100),                                             
+        @p_Dano                   varchar(100),                                             
+        @p_Quadrante              varchar(100),                                             
+        @p_Gravidade              varchar(100),                                             
+        @p_Severidade             varchar(100),                                             
+      --@p_Extensoes              varchar(100),                                             
+        @p_TipoDefeito            varchar(100), --Transporte/Fábrica/Todos                  
+        @p_DanoOrigem             varchar(100), -- Sim/Não/Todos                            
+        @p_TipoTransportador      varchar(100), -- Marítimo/Terrestre/Todos                 
+        @p_FrotaViagem            varchar(100),                                                                                    
+        @p_Navio                  varchar(100),                                             
+        @p_DataInicio             Date,                                                     
+        @p_DataFinal              Date                                                      
+                                                                                            
+set      @p_Cliente_ID             = 1
+set      @p_Chassi                 = null
+set      @p_LocalInspecao          = '*'
+set      @p_LocalCheckPoint        = '*'
+set      @p_Transportador          = '*'
+set      @p_Lote                   = null
+set      @p_Marca                  = '*'
+set      @p_Modelo                 = '*'
+set      @p_Area                   = '*'
+set      @p_Condicao               = '*'
+set      @p_Dano                   = '*'
+set      @p_Quadrante              = '*'
+set      @p_Gravidade              = '*'
+set      @p_Severidade             = '*'
+set      @p_TipoDefeito            = '*'  --|T|F|
+set      @p_DanoOrigem             = '*'  --|0|1|
+set      @p_TipoTransportador      = '*'  --|T|M|
+set      @p_FrotaViagem            = null
+set      @p_Navio                  = null
+set      @p_DataInicio             = '2017-01-01'
+set      @p_DataFinal              = '2017-04-25'
+
+
+exec InspAvaria_Cons_Summary
+    @p_Cliente_ID         ,
+    @p_Chassi             ,
+    @p_LocalInspecao      ,
+    @p_LocalCheckPoint    ,   
+    @p_Transportador      ,
+    @p_Lote               ,
+    @p_Marca              ,  
+    @p_Modelo             ,  
+    @p_Area               ,  
+    @p_Condicao           ,  
+    @p_Dano               ,  
+    @p_Quadrante          ,  
+    @p_Gravidade          ,  
+    @p_Severidade         ,  
+    @p_TipoDefeito        ,  
+    @p_DanoOrigem         ,  
+    @p_TipoTransportador  ,
+    @p_FrotaViagem        , 
+    @p_Navio              , 
+    @p_DataInicio         , 
+    @p_DataFinal         
+    
+*/
